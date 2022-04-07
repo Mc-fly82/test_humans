522ec055ebc46f5e343a1b025d46764bdb065f67
Mc-fly82 <flavius_marc@yahoo.fr>
2022-04-07 15:41:09 +0200

    📦 NEW: init

---
+++ tests.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Premier test : rédigez une fonction qui retourne le plus proche palindrome du chiffre donné
+ * Si le chiffre donné n'est pas un entier ou est  en dessous de 0, la fonction retournera "Non valide"
+ * exemples :
+ *      getPalindrome(8) => 11
+ *      getPalindrome(1029) => 1001
+ */
+
+/**
+ * Second test : nous avons une variable n
+ * Si n est un nombre entier, retourner une chaine de caractères avec des tirets '-' avant et après chaque nombre entier impair,
+ * mais ne pas commencer ou terminer la chaîne par un tiret.
+ * Si n est négatif, le signe négatif doit être supprimé.
+ * Si n n'est pas un entier, retourner une valeur vide :
+ * exemples :
+ *      popDash(274) => '2-7-4'
+ *      popDash(4615) => '46-1-5'
+ */
+
+/**
+ * Troisième test : écrivez une fonction qui trouvera tous les anagrammes d'un mot à partir d'une liste. Vous recevrez deux entrées,
+ * un mot et un tableau de mots. Vous devez retourner un tableau de tous les anagrammes ou un tableau vide s'il n'y en a pas.
+ * exemples :
+ *      getAnagrams('dbbd', ['ddbb', 'dbce', 'bbdd', 'vdvd']) => ['ddbb', 'bbdd']
+ *      getAnagrams('uaceu', ['cuazeu', 'caueu', 'uacau', 'caeus', 'uaceu']) => ['uareu', 'uaceu']
+ */
+
+/**
+ * Quatrième test : Un nombre narcissique est un nombre positif qui est la somme de ses propres chiffres,
+ * chacun élevé à la puissance du nombre de chiffres dans une base donnée. Dans ce Kata, nous nous limiterons aux nombres décimaux (base 10).
+ * exemple : prenons 153 (3 chiffres), qui est narcissique :
+ * 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+ * et 1652 (4 chiffres), qui ne l'est pas :
+ * 1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+ * Votre code doit renvoyer true ou false selon que le nombre donné est un nombre narcissique ou non.
+ */
+
+
