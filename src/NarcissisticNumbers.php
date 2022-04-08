8117901bdc8f24664b4db599bfaa459be5b59c66
Mc-fly82 <flavius_marc@yahoo.fr>
2022-04-08 02:34:08 +0200

    📦 NEW: pull in utils in NarcissisticNumbers.php

--- src/NarcissisticNumbers.php
+++ src/NarcissisticNumbers.php
@@ -2,7 +2,29 @@
 
 namespace App;
 
+/**
+ * Class NarcissisticNumbers
+ *
+ * @package App
+ */
 class NarcissisticNumbers
 {
+	protected Utils $utils;
 
+	public function __construct()
+	{
+		$this->utils = new Utils();
+	}
+
+	/**
+	 * @param int $value
+	 *
+	 * @return bool
+	 */
+	public function isNarcissistic(int $value): bool
+	{
+		// 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+
+		return true;
+	}
 }
