437e74040ca734e1cf8edfbd58ca27a2c04d0e73
Mc-fly82 <flavius_marc@yahoo.fr>
2022-04-08 12:15:21 +0200

    🐛 FIX: rename advance test suite methods prefix to match the class name

--- tests/PalindromeAdvanceTest.php
+++ tests/PalindromeAdvanceTest.php
@@ -17,113 +17,39 @@ class PalindromeAdvanceTest extends TestCase
 
 	/**
 	 * @test
-	 * @group SimplePalindromeTest
-	 */
-	public function simplePalindromeTest_is_even_return_true_when_passed_22()
-	{
-		$this->is_even_return_true_when_passed_22();
-	}
-
-	/**
-	 * @test
-	 * @group SimplePalindromeTest
-	 */
-	public function simplePalindromeTest_is_even_return_false_when_passed_21()
-	{
-		$this->is_even_return_false_when_passed_21();
-	}
-
-	/**
-	 * @test
-	 * @group SimplePalindromeTest
-	 */
-	public function simplePalindromeTest_numLengthIsEven_return_false_when_passed_100()
-	{
-		$this->numLengthIsEven_return_false_when_passed_100();
-	}
-
-	/**
-	 * @test
-	 * @group SimplePalindromeTest
-	 */
-	public function simplePalindromeTest_numLengthIsEven_return_true_when_passed_10()
-	{
-		$this->numLengthIsEven_return_true_when_passed_10();
-	}
-
-	/**
-	 * @test
-	 * @group SimplePalindromeTest
-	 */
-	public function simplePalindromeTest_getNumberMedianDigitIndex_returns_1_when_given_100()
-	{
-		$this->getNumberMedianDigitIndex_returns_1_when_given_100();
-	}
-
-	/**
-	 * @test
-	 * @group SimplePalindromeTest
-	 */
-	public function simplePalindromeTest_getNumberMedianDigitIndex_returns_minus_1_when_geven_10()
-	{
-		$this->getNumberMedianDigitIndex_returns_minus_1_when_geven_10();
-	}
-
-	/**
-	 * @test
-	 * @group SimplePalindromeTest
+	 * @group AdvancePalindromeTest
 	 * @throws Exception
 	 */
-	public function simplePalindromeTest_isPalUtil_check_if_a_even_length_number_is_a_palindrome()
-	{
-		$this->isPalUtil_check_if_a_even_length_number_is_a_palindrome();
-	}
-
-	/**
-	 * @test
-	 * @group SimplePalindromeTest
-	 * @throws Exception
-	 */
-	public function simplePalindromeTest_isPalUtil_check_if_a_odd_length_number_is_a_palindrome()
-	{
-		$this->isPalUtil_check_if_a_odd_length_number_is_a_palindrome();
-	}
-
-	/**
-	 * @test
-	 * @group SimplePalindromeTest
-	 * @throws Exception
-	 */
-	public function simplePalindromeTest_isPal_throw_exception_on_negative_number()
+	public function advancePalindromeTest_isPal_throw_exception_on_negative_number()
 	{
 		$this->isPal_throw_exception_on_negative_number();
 	}
 
 	/**
 	 * @test
-	 * @group SimplePalindromeTest
+	 * @group AdvancePalindromeTest
 	 * @throws Exception
 	 */
-	public function simplePalindromeTest_isPal_pass()
+	public function advancePalindromeTest_isPal_pass()
 	{
 		$this->isPal_pass();
 	}
 
 	/**
 	 * @test
-	 * @group SimplePalindromeTest
+	 * @group AdvancePalindromeTest
 	 */
-	public function simplePalindromeTest_isPal_fails()
+	public function advancePalindromeTest_isPal_fails()
 	{
 		$this->isPal_fails();
 	}
 
 	/**
 	 * @test
-	 * @group SimplePalindromeTest
+	 * @group AdvancePalindromeTest
 	 * @throws Exception
 	 */
-	public function simplePalindromeTest_findNextPalindrome_test()
+	public function advancePalindromeTest_findNextPalindrome_test()
 	{
 		$this->findNextPalindrome_test();
 	}
