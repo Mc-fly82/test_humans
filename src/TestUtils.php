f34acee11343d09d277eeff4bfd3278f87832ead
Mc-fly82 <flavius_marc@yahoo.fr>
2022-04-08 02:16:27 +0200

    📦 NEW: add TestUtils.php trait

---
+++ src/TestUtils.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+/**
+ * Trait TestUtils
+ *
+ * @package App
+ */
+trait TestUtils
+{
+	/**
+	 * Asserts that two associative arrays are similar.
+	 *
+	 * Both arrays must have the same indexes with identical values
+	 * without respect to key ordering
+	 *
+	 * @param array $expected
+	 * @param array $array
+	 */
+	protected function assertArraySimilar(array $expected, array $array)
+	{
+		$this->assertTrue(count(array_diff_key($array, $expected)) === 0);
+
+		foreach ($expected as $key => $value) {
+			if (is_array($value)) {
+				$this->assertArraySimilar($value, $array[ $key ]);
+			} else {
+				$this->assertContains($value, $array);
+			}
+		}
+	}
+}
